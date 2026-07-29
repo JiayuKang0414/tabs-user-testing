8625e1a402745af440edf740770be545c4a47318
jiayukangumd <108474962+jiayukangumd@users.noreply.github.com>
2026-07-29 15:53:30 -0400

    Publish tabs user testing pages

---
+++ scripts/tabs-user-testing.js
@@ -0,0 +1,27 @@
+(function () {
+  var host = document.querySelector('.qa-tabs-c');
+  if (!host) return;
+
+  function injectVersionCStyles() {
+    if (!host.shadowRoot) {
+      window.requestAnimationFrame(injectVersionCStyles);
+      return;
+    }
+
+    if (host.shadowRoot.querySelector('[data-tabs-version-c-styles]')) return;
+
+    var style = document.createElement('style');
+    style.setAttribute('data-tabs-version-c-styles', '');
+    style.textContent = [
+      '.tab-active-line {',
+      '  animation: none !important;',
+      '  display: block !important;',
+      '  transition: none !important;',
+      '}'
+    ].join('\n');
+
+    host.shadowRoot.appendChild(style);
+  }
+
+  customElements.whenDefined('umd-element-tabs').then(injectVersionCStyles);
+})();
